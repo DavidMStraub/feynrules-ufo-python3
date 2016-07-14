b99411c03cbfc000d7f8c9599fcc1e8c69f3f88b
David Straub <david.straub@tum.de>
2016-07-14 11:46:29 +0200

    Adapting manual imports

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,11 +1,11 @@
 
 from __future__ import absolute_import
-import particles
-import couplings
-import lorentz
-import parameters
-import vertices
-import coupling_orders
+from . import particles
+from . import couplings
+from . import lorentz
+from . import parameters
+from . import vertices
+from . import coupling_orders
 from . import write_param_card
 from . import propagators
 
@@ -20,21 +20,21 @@ all_functions = function_library.all_functions
 all_propagators = propagators.all_propagators
 
 try:
-   import decays
+   from . import decays
 except ImportError:
    pass
 else:
    all_decays = decays.all_decays
 
 try:
-   import form_factors
+   from . import form_factors
 except ImportError:
    pass
 else:
    all_form_factors = form_factors.all_form_factors
 
 try:
-   import CT_vertices
+   from . import CT_vertices
 except ImportError:
    pass
 else:
